f226578bae3deb894c74e04b1282351f570bd2a3
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2022-05-13 16:17:58 +0200

    [Add]: basic template code

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+// rollup.config.js
+import json from '@rollup/plugin-json';
+import eslint from '@rollup/plugin-eslint';
+import typescript from '@rollup/plugin-typescript';
+import run from '@rollup/plugin-run';
+
+// Import dependencies
+import { dependencies } from "./package.json"
+
+const preferConst = true; // Use "const" instead of "var"
+const isWatched = process.env.ROLLUP_WATCH === 'true'; // `true` if -w option is used
+
+export default {
+  external: dependencies ? [...Object.keys(dependencies)] : [],
+  input: 'src/app.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+    preferConst: preferConst,
+    preserveModules: false,
+    strict: true,
+    entryFileNames: "[name].mjs"
+  },
+  plugins: [
+    json({
+      preferConst: preferConst
+    }),
+    eslint({
+      throwOnError: true
+    }),
+    typescript(), 
+    isWatched ? run() : undefined
+  ]
+};
