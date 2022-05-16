949ff77d7f15cbe88d53126d2db54289c56810d4
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2022-05-16 10:21:18 +0200

    v0.1.2 (#4)
    * [Update]: solve issue with jest and ESM modules
    
    * [Add]: documentation through typedoc

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,23 @@
 // jest.config.js
-const { join } = require('path')
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig.json')
+import { join } from 'path'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import tsconfig from './tsconfig.json' assert {type: 'json'};
 
-module.exports = {
+const { compilerOptions } = tsconfig;
+
+export default {
   preset: 'ts-jest', // Enable typescript support
   testEnvironment: 'node', // We are in node
   verbose: true, // Versbose output
   collectCoverage: true, // Show also coverage
   coverageProvider: "v8", // Generate file using the v8 engine
+  extensionsToTreatAsEsm: ['.ts'],
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json', // Config gile
+      tsconfig: compilerOptions, // Config file
+      useESM: true,
     },
   },
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: join('<rootDir>', compilerOptions.baseUrl) }) // Map aliases to their path
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: join('<rootDir>', compilerOptions.baseUrl) }), // Map aliases to their path
+  transform: {}
 }
