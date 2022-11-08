7d737fe94eacd350c34b8683c3a54d6de3b11709
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2022-11-08 19:46:55 +0100

    0.5.0 (#16)
    * [Update]: use esbuild for bundling
    
    * [Chore]: bump version to 0.5.0

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,9 +3,16 @@ import json from '@rollup/plugin-json';
 import eslint from '@rollup/plugin-eslint';
 import typescript from '@rollup/plugin-typescript';
 import run from '@rollup/plugin-run';
-import externals from 'rollup-plugin-node-externals'
+import externals from 'rollup-plugin-node-externals';
+import esbuild from 'rollup-plugin-esbuild';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 
 const preferConst = true; // Use "const" instead of "var"
+const usePreserveModules = true; // `true` -> keep modules structure, `false` -> combine everything into a single file
+const useThrowOnError = true; // On error throw and exception
+const useEsbuild = true; // `true` -> use esbuild, `false` use tsc
+
+const isCli = false; // `true` -> is a CLI so bunlde to a single file, `false` not a cli, so use `usePreserveModules`
 const isWatched = process.env.ROLLUP_WATCH === 'true'; // `true` if -w option is used
 
 export default {
@@ -16,19 +23,30 @@ export default {
     generatedCode: {
       constBindings: preferConst,
     },
-    preserveModules: true,
+    preserveModules: isCli ? false : usePreserveModules,
     strict: true,
     entryFileNames: '[name].mjs',
+    banner: isCli ? '#!/usr/bin/env node' : undefined,
   },
   plugins: [
     externals(),
     eslint({
-      throwOnError: true,
+      throwOnError: useThrowOnError,
     }),
     json({
       preferConst: preferConst,
     }),
-    typescript(),
+    useEsbuild
+      ? [
+          typescriptPaths({
+            preserveExtensions: true,
+          }),
+          esbuild(),
+        ]
+      : typescript({
+          noEmitOnError: useThrowOnError,
+          removeComments: true,
+        }),
     isWatched ? run() : undefined,
   ],
 };
